c763fae142b36f273cd0dc7c7b7c2e8766185eb3
dz129 <danielzhang129@gmail.com>
2024-01-11 22:41:02 -0500

    Initial Commit

---
+++ CityNavigation/CityNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityNavigationApp.swift
+//  CityNavigation
+//
+//  Created by Daniel Zhang on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CityNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityNavigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CityNavigation
+//
+//  Created by Daniel Zhang on 1/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
